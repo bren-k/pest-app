fc8de468666daf285b68e33e92f72ddf02917ecd
bren-k <45498578+bren-k@users.noreply.github.com>
2018-12-07 11:09:35 +1000

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -36,6 +36,61 @@ function initApplication() {
             sectionHeading.textContent = "Record Location";
         });
     });
+
+    let photo_input = document.getElementById("pest_photo");
+    let photo_preview = document.qgetElementById("preview");
+
+
+    photo_input.addEventListener("change", displayPreview);
+
+    function displayPreview() {
+        //remove existing preview (if any)
+        if (photo_preview.firstChild) {
+            photo_preview.removeChild(photo_preview.firstChild);
+        }
+
+        let curFiles = photo_input.files;
+        if(curFiles.length === 0) {
+            photo_preview.textContent = "No Photo";
+        } 
+        else {
+            photo_preview.textContent = "";
+            if(validFileType(curFiles[0])) {
+                //para.textContent = 'File name ' + curFiles[0].name + ', file size ' + returnFileSize(curFiles[0].size) + '.';
+                var image = document.createElement("img");
+                image.src = window.URL.createObjectURL(curFiles[0]);
+
+                photo_preview.appendChild(image);
+            } 
+            else {
+                console.log("File name " + curFiles[0].name + ": Not a valid file type.");
+            }
+        }
+    }//end displayPreview() function
+
+let fileTypes = [
+  "image/jpeg",
+  "image/png"
+]
+
+function validFileType(file) {
+  for(let i = 0; i < fileTypes.length; i++) {
+    if(file.type === fileTypes[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function returnFileSize(number) {
+  if(number < 1024) {
+    return number + "bytes";
+  } else if(number >= 1024 && number < 1048576) {
+    return (number/1024).toFixed(1) + "KB";
+  } else if(number >= 1048576) {
+    return (number/1048576).toFixed(1) + "MB";
+  }
+}
     
 
 }//end initApplication() function
